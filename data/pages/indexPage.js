4d34e95da37e235d31b99b58c165bce51917b6a4
jimenaCastro73 <jimenacastro0305@gmail.com>
2025-04-07 16:44:58 -0600

    fix: rutas de nuevo en indexPage

--- data/pages/indexPage.js
+++ data/pages/indexPage.js
@@ -2,9 +2,9 @@ export const indexPage = {
     inicio: {
         title: "Tecnitours - Tu Agencia de Viajes en Honduras",
         heroPanel: {
-            desktop: "/public/imgs/heroPanel/paisaje-1(Desktop).jpg",
-            tablet: "/public/imgs/heroPanel/paisaje-3(Tablets).jpg",
-            mobile: "/public/imgs/heroPanel/paisaje-2(movil).jpg",
+            desktop: "/imgs/heroPanel/paisaje-1(Desktop).jpg",
+            tablet: "/imgs/heroPanel/paisaje-3(Tablets).jpg",
+            mobile: "/imgs/heroPanel/paisaje-2(movil).jpg",
             altText: "Paisaje",
             overlayText: "Un nuevo concepto en viajes"
         },
@@ -12,62 +12,62 @@ export const indexPage = {
             titulo: "Destinos destacados desde Tegucigalpa",
             tarjetas: [
                 {
-                    imagen: "/public/imgs/paisajes/machu picchu.jpg",
+                    imagen: "/imgs/paisajes/machu picchu.jpg",
                     titulo: "Machu Picchu, Perú",
                     descripcion: "El país andino guarda en su territorio tantas joyas que sería imposible enumerarlas una a una."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/petra.jpg",
+                    imagen: "/imgs/paisajes/petra.jpg",
                     titulo: "Petra, Jordania",
                     descripcion: "Petra, uno de los lugares más turísticos del mundo, es una joya hecha ciudad"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/chichen itza.JPG",
+                    imagen: "/imgs/paisajes/chichen itza.JPG",
                     titulo: "Chichen Itzá, México",
                     descripcion: "Uno de los sitios arqueológicos más famosos de la civilización maya, conocido por su pirámide principal, El Castillo."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/acropolis.jpg",
+                    imagen: "/imgs/paisajes/acropolis.jpg",
                     titulo: "La Acrópolis, Grecia",
                     descripcion: "Una antigua ciudadela situada en una colina en Atenas. Alberga el Partenón, un templo dedicado a la diosa Atenea."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/piramides giza.jpg",
+                    imagen: "/imgs/paisajes/piramides giza.jpg",
                     titulo: "Las Pirámides de Giza, Egipto",
                     descripcion: "Monumentos icónicos de la antigua civilización egipcia. Incluyen la Gran Pirámide de Keops y la Esfinge"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/taj mahal.jpeg",
+                    imagen: "/imgs/paisajes/taj mahal.jpeg",
                     titulo: "El Taj Mahal, India",
                     descripcion: "Una majestuosa obra de arquitectura mogol construida como mausoleo en honor a Mumtaz Mahal, la esposa del emperador Shah Jahan."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/torre eiffel.jpg",
+                    imagen: "/imgs/paisajes/torre eiffel.jpg",
                     titulo: "La Torre Eiffel, Francia",
                     descripcion: "Un ícono mundial de París y Francia. Esta torre de hierro, diseñada por Gustave Eiffel."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/la alhambra.jpg",
+                    imagen: "/imgs/paisajes/la alhambra.jpg",
                     titulo: "La Alhambra, España",
                     descripcion: "Un conjunto de palacios, fortalezas y jardines ubicado en Granada."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/coliseo.jpg",
+                    imagen: "/imgs/paisajes/coliseo.jpg",
                     titulo: "Coliseo, Italia",
                     descripcion: "Un anfiteatro en Roma donde se celebraban espectáculos de gladiadores y eventos públicos."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/time square.jpg",
+                    imagen: "/imgs/paisajes/time square.jpg",
                     titulo: "Times Square, Estados Unidos",
                     descripcion: "Una intersección vibrante en Nueva York, conocida como 'la encrucijada del mundo'."
                 },
                 {
-                    imagen: "/public/imgs/paisajes/stonehenge.jpg",
+                    imagen: "/imgs/paisajes/stonehenge.jpg",
                     titulo: "Stonehenge, Reino Unido",
                     descripcion: "Un misterioso círculo de piedras prehistóricas en Inglaterra. Se cree que fue utilizado para rituales religiosos"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/cristo redentor.jpg",
+                    imagen: "/imgs/paisajes/cristo redentor.jpg",
                     titulo: "Cristo Redentor, Brasil",
                     descripcion: "Una gigantesca estatua de Jesús en la cima del Cerro del Corcovado en Río de Janeiro."
                 }
@@ -78,17 +78,17 @@ export const indexPage = {
             descripcion: "Descubre quiénes somos y lo que hacemos para ofrecerte la mejor experiencia de viaje.",
             secciones: [
                 {
-                    imagen: "/public/imgs/paisajes/nosotros.jpg",
+                    imagen: "/imgs/paisajes/nosotros.jpg",
                     titulo: "Sobre Nosotros",
                     url: "pages/about.html"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/blog.jpg",
+                    imagen: "/imgs/paisajes/blog.jpg",
                     titulo: "Blog",
                     url: "pages/blog.html"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/contacto.jpg",
+                    imagen: "/imgs/paisajes/contacto.jpg",
                     titulo: "Contacto",
                     url: "pages/contacto.html"
                 }
@@ -98,19 +98,19 @@ export const indexPage = {
             titulo: "Ofertas Destacadas",
             items: [
                 {
-                    imagen: "/public/imgs/paisajes/cancun.jpg",
+                    imagen: "/imgs/paisajes/cancun.jpg",
                     titulo: "Escapada de Lujo en Cancún",
                     descripcion: "Disfruta de una estancia de 5 noches en uno de los mejores resorts de Cancún. Incluye todo incluido: alimentos, bebidas y acceso a las mejores actividades de playa y entretenimiento nocturno. ¡Perfecto para una escapada romántica o en familia!",
                     url: "pages/servicioHoteles.html"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/avion.jpg",
+                    imagen: "/imgs/paisajes/avion.jpg",
                     titulo: "Vuelos Directos a Europa",
                     descripcion: "Aprovecha esta oferta exclusiva para volar directo a Europa desde Tegucigalpa con tarifas especiales. Disfruta de una experiencia de vuelo cómoda y segura, ¡y comienza tu aventura por ciudades como Madrid, París y Roma!",
                     url: "pages/servicioVuelos.html"
                 },
                 {
-                    imagen: "/public/imgs/paisajes/crucero.jpg",
+                    imagen: "/imgs/paisajes/crucero.jpg",
                     titulo: "Crucero por el Caribe: 2x1 en boletos",
                     descripcion: "Vive la experiencia de un exclusivo crucero por el Caribe. Disfruta de 7 días a bordo de un lujoso barco, visitando destinos paradisíacos, con todo incluido: entretenimiento, restaurantes y actividades para todos los gustos.",
                     url: "pages/servicioPaquetes.html"
