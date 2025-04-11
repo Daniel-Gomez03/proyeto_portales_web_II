adc7b6e68837fd134d85e511ef021b9d225a2d44
LUCIA SIERRA MENDOZA <luciaardilla@hotmail.com>
2025-04-11 01:03:40 -0600

    feat: data y partials de Hoteles y Vuelos

--- data/pages/servicioHotelesPage.js
+++ data/pages/servicioHotelesPage.js
@@ -1,3 +1,128 @@
 export const servicioHotelesPage = {
+    title: "Hoteles - Tecnitours",
+    hoteles:{
+        titulo: "Encuentra tu estadía ideal",
+        descripcion: "Explora nuestra selección de hoteles cuidadosamente elegidos para brindarte comodidad y calidad en cada destino. Desde alojamientos acogedores hasta resorts de lujo, Tecnitours te conecta con opciones que se adaptan a tus necesidades y te acercan a vivir experiencias inolvidables. Haz de tu próximo viaje un recuerdo excepcional con la estadía perfecta",
+        hero_panel_hotel:{
+            img: "/imgs/heroPanel/hotel-hero.jpg",
+            altText: "Atardecer en la Playa",
+            enlaceDesc:"Más Opciones"
+        },
 
+        sliderHero:[
+            {
+                img: "/imgs/sección-hoteles/Hotel-Italia.jpg",
+                altText: "Hotel en Italia",
+                titulo: "Italia"
+            },
+            {
+                img: "/imgs/sección-hoteles/Hotel-CanadaVancouver.jpg",
+                altText: "Hotel en Canadá",
+                titulo: "Canadá"
+            },
+            {
+                img: "/imgs/sección-hoteles/Hotel-Suiza.png",
+                altText: "Hotel en Suiza",
+                titulo: "Suiza"
+            },
+            {
+                img: "/imgs/sección-hoteles/Hotel-USA.jpg",
+                altText: "Hotel en USA",
+                titulo: "Estados Unidos"
+            },
+            {
+                img: "/imgs/sección-hoteles/Hotel-JP.jpg",
+                altText: "Hotel en Japón",
+                titulo: "Japón"
+            }
+        ],
+
+        formularioHotel:{
+            campos:{
+                inputs:[
+                    {
+                        id: "destino",
+                        tipo: "text",
+                        nombre: "destino",
+                        etiqueta: "Destino:",
+                        placeholder: "País, ciudad o región",
+                        pattern: "[A-Za-z\´s]+",
+                        title: "Solo se permiten letras y espacios",
+                        descripcion: "¿A dónde se dirige?"
+                    },
+                ],
+                select:{
+                    id: "cmbTipoHabitacion",
+                    nombre: "cmbTipoHabitacion",
+                    etiqueta: "Tipo de Habitación:",
+                    descripcion: "Seleccione el tipo de habitación",
+                    opciones:[
+                        {valor: "1", texto: "Sencilla"},
+                        {valor: "2", texto: "Doble"},
+                        {valor: "3", texto: "Triple"},
+                        {valor: "4", texto: "Cuádruple"},
+                        {valor: "5", texto: "Quíntuple"},
+                        {valor: "6", texto: "Sextuple"},
+                    ]
+                }
+            }
+        },
+        
+        recomendaciones:[
+            {
+                img1: "/imgs/sección-hoteles/Indura Beach.jpg",
+                img2: "/imgs/sección-hoteles/Indura Beach3.jpg",
+                img3: "/imgs/sección-hoteles/Indura Beach4.jpg",
+                altText: "Hotel Indura Beach",
+                titulo: "Indura Beach & Golf Resort",
+                location: "Tela, Honduras",
+                precio: "$575.00"
+            },
+            {
+                img1: "/imgs/sección-hoteles/Hilton.jpg",
+                img2: "/imgs/sección-hoteles/Hilton2.jpg",
+                img3: "/imgs/sección-hoteles/Hilton3.jpg",
+                altText: "Hotel Hilton Vacation Club Grand",
+                titulo: "Hilton Vacation Club Grand",
+                location: "Orlando, Estados Unidos",
+                precio: "$348.00"
+            },
+            {
+                img1: "/imgs/sección-hoteles/Palma Real.jpg",
+                img2: "/imgs/sección-hoteles/Palma Real 2.jpg",
+                img3: "/imgs/sección-hoteles/Palma Real3.jpg",
+                altText: "Hotel Palma Real",
+                titulo: "Palma Real",
+                location: "La Ceiba, Honduras",
+                precio: "$255.00"
+            },
+            {
+                img1: "/imgs/sección-hoteles/SinaVille.jpg",
+                img2: "/imgs/sección-hoteles/SinaVille2.jpg",
+                img3: "/imgs/sección-hoteles/SinaVille3.jpg",
+                altText: "Hotel Sina De la Ville",
+                titulo: "Sina De la Ville",
+                location: "Milán, Lombardía, Italia",
+                precio: "$428.00"
+            },
+            {
+                img1: "/imgs/sección-hoteles/Mandarin Oriental.jpg",
+                img2: "/imgs/sección-hoteles/Mandarin Oriental2.jpg",
+                img3: "/imgs/sección-hoteles/Mandarin Oriental3.jpg",
+                altText: "Hotel Mandarin Oriental",
+                titulo: "Mandarin Oriental",
+                location: "Santiago, Chile",
+                precio: "$399.00"
+            },
+            {
+                img1: "/imgs/sección-hoteles/Hotel Camino Real Antigua.jpg",
+                img2: "/imgs/sección-hoteles/Hotel Camino Real Antigua2.jpg",
+                img3: "/imgs/sección-hoteles/Hotel Camino Real Antigua3.jpg",
+                altText: "Hotel Camino Real Antigua",
+                titulo: "Hotel Camino Real Antigua",
+                location: "Antigua Guatemala, Sacatepéquez, Guatemala",
+                precio: "$291.00"
+            }
+        ]
+    }
 } 
--- data/pages/servicioVuelosPage.js
+++ data/pages/servicioVuelosPage.js
@@ -1,3 +1,148 @@
 export const servicioVuelosPage = {
+    title: "Vuelos - Tecnitours",
+    vuelos:{
+        titulo: "Viaja para explorar el Mundo",
+        descripcion: "De la mano de Tecnitours, haz realidad tus sueños de aventura y descubre nuevos destinos alrededor del mundo. Con nuestras opciones de vuelos, te llevamos a donde siempre has querido ir, brindándote seguridad, comodidad y atención personalizada en cada paso de tu viaje. Prepárate para volar y crear recuerdos inolvidables.",
+        hero_panel_vuelo:{
+            img: "/imgs/sección-vuelo/Paisaje.jpg",
+            altText: "Paisaje tropical",
+        },
 
+        sugerencias:{
+            titulo: "Destinos Populares",
+            subtitulo: "Descubre Lugares",
+            descripcion: "Explora los lugares que todos quieren visitar y déjate inspirar por la belleza de ciudades icónicas, paisajes espectaculares y culturas vibrantes. En Tecnitours, hemos seleccionado los destinos más populares para que comiences a planear tu próxima gran aventura."
+        },
+
+        formularioVuelo:{
+            titulo: "✈️Programa tu Vuelo",
+            campos:{
+                inputs:[
+                    {
+                        id: "origen",
+                        tipo: "text",
+                        nombre: "origen",
+                        etiqueta: "Desde:",
+                        placeholder: "Ingrese Origen",
+                    },
+                    {
+                        id: "destino",
+                        tipo: "text",
+                        nombre: "destino",
+                        etiqueta: "Hacia:",
+                        placeholder: "Ingrese Destino",
+                    },
+                    {
+                        id: "personas",
+                        tipo: "number",
+                        nombre: "personas",
+                        etiqueta: "Número de Pasajeros:",
+                        placeholder: "No. de personas",
+                    },
+                ],
+                fechas:[
+                    {
+                        id: "fecha_salida",
+                        tipo: "date",
+                        nombre: "fecha_salida",
+                        etiqueta: "Fecha de Salida:",
+                    },
+                    {
+                        id: "fecha_retorno",
+                        tipo: "date",
+                        nombre: "fecha_retorno",
+                        etiqueta: "Fecha de Retorno:",
+                    }
+                ],
+                select:{
+                    id:"cmbTipoClase",
+                    nombre: "cmbTipoClase",
+                    etiqueta: "Clase:",
+                    opciones:[
+                        {valor:"Eco", texto:"Económica"},
+                        {valor:"Eje", texto:"Ejecutiva"},
+                        {valor:"1Cla", texto:"Primera Clase"}
+                    ]
+                }
+            }
+        },
+
+        recomendaciones:[
+            {
+                img: "/imgs/sección-vuelo/París.jpg",
+                altText: "París, Francia",
+                loading: "lazy",
+                rating: "★★★★★",
+                referencia: "/pages/servicioVuelos.html#vuelos-hero",
+                titulo: "Francia",
+                subtitulo: "París",
+                descripcion: "La Ciudad de la Luz te espera con su incomparable romanticismo, icónicos monumentos como la Torre Eiffel, el Louvre y sus encantadoras calles llenas de historia y arte. Vive la magia de París y su exquisita gastronomía."
+            },
+            {
+                img: "/imgs/sección-vuelo/Orlando-Florida.jpg",
+                altText: "Orlando-Florida, Estados Unidos",
+                loading: "lazy",
+                rating: "★★★★★",
+                referencia: "/pages/servicioVuelos.html#vuelos-hero",
+                titulo: "Estados Unidos",
+                subtitulo: "Orlando-Florida",
+                descripcion: "El corazón de la diversión en familia. Hogar de los parques temáticos más famosos del mundo, Orlando combina aventura, entretenimiento y sol, ofreciendo experiencias únicas para todas las edades."
+            },
+            {
+                img: "/imgs/sección-vuelo/Venecia.jpg",
+                altText: "Venecia, Italia",
+                loading: "lazy",
+                rating: "★★★★★",
+                referencia: "/pages/servicioVuelos.html#vuelos-hero",
+                titulo: "Italia",
+                subtitulo: "Venecia",
+                descripcion: "La ciudad flotante que parece sacada de un sueño. Recorre sus pintorescos canales en góndola, maravíllate con su arquitectura renacentista y disfruta de la singular belleza de este destino lleno de historia y romance."
+            }
+        ],
+
+        paquete:{
+            titulo: "Echa un vistazo a nuestros paquetes",
+            subtitulo: "Paquetes Sugeridos",
+            descripcion: "En Tecnitours, hemos diseñado paquetes de vuelo que se adaptan a tus necesidades y sueños. Ya sea que busques una escapada corta, unas vacaciones familiares o una aventura internacional, nuestros paquetes ofrecen la mejor selección de vuelos a destinos increíbles con comodidad, flexibilidad y precios competitivos. ¡Elige tu destino y comienza a volar hoy mismo!"
+        },
+
+        paquetesLista:[
+            {
+                img: "/imgs/servicios/paquete2.jpg",
+                altText: "Paquete Aérero Roatán",
+                loading: "lazy",
+                titulo: "Escapada a la playa: 3 días y 2 noches de relax",
+                descripcion: "Escápate a la tranquilidad del mar con nuestro paquete de 3 días y 2 noches en la playa. Disfruta de la brisa marina, relájate bajo el sol y recarga energías en un destino paradisíaco. Ideal para desconectar de la rutina y vivir una experiencia inolvidable junto al océano.",
+                comments: "(20 comentarios)",
+                rating: "★★★★★",
+                precio: "$670 / $590",
+                span: "*por persona",
+                boton: "Reservar Ahora"
+            },
+            {
+                img: "/imgs/servicios/paquete3.jpg",
+                altText: "Paquete del Casabe",
+                loading: "lazy",
+                titulo: "Aventura en La Ceiba: 4 días y 3 noches de exploración",
+                descripcion: "Descubre la magia de La Ceiba en nuestro paquete de 4 días y 3 noches. Rodeado de naturaleza exuberante, desde sus playas hasta sus parques nacionales, vivirás una experiencia llena de aventura, cultura y relajación. Disfruta de actividades emocionantes y del encanto único de esta ciudad caribeña, ideal para los amantes de la naturaleza y los viajes llenos de descubrimientos.",
+                comments: "(15 comentarios)",
+                rating: "★★★★☆",
+                precio: "$345",
+                span: "*por persona",
+                boton: "Reservar Ahora"
+            },
+            {
+                img: "/imgs/servicios/Paquete4.jpeg",
+                altText: "Paquete Disney",
+                loading: "lazy",
+                titulo: "Magia en Disneyland",
+                descripcion: "Descubre el mundo encantado de Disneyland California, donde la magia cobra vida. Vive la emoción de los parques temáticos, conoce a tus personajes favoritos y disfruta de atracciones únicas que solo Disney puede ofrecer. Una experiencia mágica y llena de diversión para toda la familia, ideal para crear recuerdos que perdurarán toda la vida.",
+                comments: "(35 comentarios)",
+                rating: "★★★★★",
+                precio: "$759",
+                span: "*por persona",
+                boton: "Reservar Ahora"
+            }
+        ]
+    }
 } 
